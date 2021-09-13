f39b009b070974bd5b20589b9b9d35dadc0da259
tororo_1066 <71592738+tororo1066@users.noreply.github.com>
2021-09-13 17:28:25 +0900

    ログが見えたよやったー

--- src/main/java/tororo1066/coinflip/CoinFlipLog.kt
+++ src/main/java/tororo1066/coinflip/CoinFlipLog.kt
@@ -24,39 +24,34 @@ class CoinFlipLog{
      * @return logのデータ 存在しない場合は空っぽ
      */
     fun showlog(uuid : UUID) : CoinFlipLog{
-
-        val logdata = CoinFlipLog()
-
-        CoinFlip.es.execute {
-            val rs = mysql.query("select * from coinflip_player_log where uuid = '$uuid'")
-            if (rs == null){
-                mysql.close()
-                return@execute
-            }
-            if (!rs.next()) {
-                rs.close()
-                mysql.close()
-                return@execute
-            }
-            logdata.uuid = UUID.fromString(rs.getString("uuid"))
-            logdata.mcid = rs.getString("mcid")
-            logdata.win = rs.getInt("win")
-            logdata.lose = rs.getInt("lose")
-            logdata.wincollect = rs.getDouble("wincollect")
-            logdata.losecollect = rs.getDouble("losecollect")
-            logdata.maxcollect = rs.getDouble("maxcollect")
-            if (logdata.win == 0 || logdata.lose == 0){
-                logdata.winingpersentage = 0.0
-            }else{
-                logdata.winingpersentage = floor((logdata.win.toDouble() / (logdata.win.toDouble() + logdata.lose.toDouble())) * 100)
-            }
-            logdata.success = true
-
+        val rs = mysql.query("select * from coinflip_player_log where uuid = '$uuid'")
+        if (rs == null){
+            mysql.close()
+            return CoinFlipLog()
+        }
+        if (!rs.next()) {
             rs.close()
             mysql.close()
-
-            return@execute
+            return CoinFlipLog()
         }
+        val logdata = CoinFlipLog()
+        logdata.uuid = UUID.fromString(rs.getString("uuid"))
+        logdata.mcid = rs.getString("mcid")
+        logdata.win = rs.getInt("win")
+        logdata.lose = rs.getInt("lose")
+        logdata.wincollect = rs.getDouble("wincollect")
+        logdata.losecollect = rs.getDouble("losecollect")
+        logdata.maxcollect = rs.getDouble("maxcollect")
+        if (logdata.win == 0 || logdata.lose == 0){
+            logdata.winingpersentage = 0.0
+        }else{
+            logdata.winingpersentage = floor((logdata.win.toDouble() / (logdata.win.toDouble() + logdata.lose.toDouble())) * 100)
+        }
+        logdata.success = true
+
+        rs.close()
+        mysql.close()
+
         return logdata
     }
 
--- src/main/java/tororo1066/coinflip/commands/ShowLogOp.kt
+++ src/main/java/tororo1066/coinflip/commands/ShowLogOp.kt
@@ -10,20 +10,23 @@ import tororo1066.coinflip.CoinFlipLog
 class ShowLogOp : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
 
-        val logdata = CoinFlipLog().showlog(Bukkit.getOfflinePlayer(args[1]).uniqueId)
+        CoinFlip.es.execute {
+            val logdata = CoinFlipLog().showlog(Bukkit.getOfflinePlayer(args[1]).uniqueId)
 
-        if (!logdata.success){
-            CoinFlip.sendMsg(sender,"§4データが存在しません")
-            return true
+            if (!logdata.success){
+                CoinFlip.sendMsg(sender,"§4データが存在しません")
+                return@execute
+            }
+
+            sender.sendMessage("§e========${logdata.mcid}の戦績========")
+            sender.sendMessage("§c勝利数：${logdata.win}回")
+            sender.sendMessage("§b敗北数：${logdata.lose}回")
+            sender.sendMessage("§d勝率：${logdata.winingpersentage}%(切り捨て)")
+            sender.sendMessage("${if (logdata.wincollect-logdata.losecollect < 0) "§c" else "§b"}総獲得賞金：${CoinFlip.format(logdata.wincollect-logdata.losecollect)}円")
+            sender.sendMessage("§a最大獲得賞金：${CoinFlip.format(logdata.maxcollect)}円")
+            sender.sendMessage("§e========${logdata.mcid}の戦績========")
+            return@execute
         }
-
-        sender.sendMessage("§e========${logdata.mcid}の戦績========")
-        sender.sendMessage("§c勝利数：${logdata.win}回")
-        sender.sendMessage("§b敗北数：${logdata.lose}回")
-        sender.sendMessage("§d勝率：${logdata.winingpersentage}%(切り捨て)")
-        sender.sendMessage("${if (logdata.wincollect-logdata.losecollect < 0) "§c" else "§b"}総獲得賞金：${CoinFlip.format(logdata.wincollect-logdata.losecollect)}円")
-        sender.sendMessage("§a最大獲得賞金：${CoinFlip.format(logdata.maxcollect)}円")
-        sender.sendMessage("§e========${logdata.mcid}の戦績========")
 
         return true
     }
